c7a7a5ed679af50dc4006abbc4c556803927e5c5
Scott <ccssggaa@gmail.com>
2021-09-07 16:23:56 -0500

    Building bot

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import os
-from dotenv import load_dotenv
 from discord.ext import commands
-
-load_dotenv()
-token = os.getenv('discord_token')
 bot = commands.Bot(command_prefix ='/')
 @bot.command(name='precio')
 async def consulta_precio(ctx, nombre):
@@ -17,4 +13,4 @@ async def consulta_precio(ctx, nombre):
         texto="Vehículo no encontrado"
 
     await ctx.send(texto)
-bot.run(token)
+bot.run(os.environ('tokendiscord'))

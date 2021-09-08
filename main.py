5ba43bab87ff2bba7b213d43de748e832e6d1e17
Scott <ccssggaa@gmail.com>
2021-09-07 23:40:55 -0500

    update

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ bot = commands.Bot(command_prefix ='/')
 @bot.command(name='precio')
 async def consulta_precio(ctx, nombre):
     lower = nombre.lower()
-    texto = ""
-    if lower == "double":
-        texto = "Ah, como chingas con la Double, te consigues esa madre en 50mil."
+    if lower == "infernus2":
+        texto="- Tipo = SportClasico \n- Precio Total ="+int(int(110000)+int(3500)*hierro+int(1167)*plastico+int(875)*vidrio+int(20000)+int(20000))
     elif lower == "":
         texto=" "
     elif lower == "calleja":
@@ -24,6 +23,6 @@ async def consulta_precio(ctx, nombre):
     await ctx.send(texto)
 @bot.command(name='exclusivos')
 async def consulta_ex(ctx):
-    texto = "**Vehículos exclusivos de la semana.**_- \nBaller 6 \n- Blazer 3\n- Chimera\n- Dukes 3\n- Issi 7\n- Novak\n- Seasparrow 2_"
+    texto = "**Vehículos exclusivos de la semana.**_\n- Baller 6 \n- Blazer 3\n- Chimera\n- Dukes 3\n- Issi 7\n- Novak\n- Seasparrow 2_"
     await ctx.send(texto)
 bot.run(os.environ['tokendiscord'])

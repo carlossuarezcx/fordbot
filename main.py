e5a6d33e542c1a8479f0dcbc591a6471a6959f92
Scott <ccssggaa@gmail.com>
2021-09-07 22:17:57 -0500

    Building bot

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 from discord.ext import commands
+hierro = 50
+plastico = 210
+vidrio = 210
 bot = commands.Bot(command_prefix ='/')
 @bot.command(name='precio')
 async def consulta_precio(ctx, nombre):
     lower = nombre.lower()
     texto = ""
     if lower == "double":
-        texto = ""
+
+        texto = " "
     elif lower == "infernus2":
         texto=" "
     elif lower == "lavin":
@@ -16,5 +20,9 @@ async def consulta_precio(ctx, nombre):
     else:
         texto="Vehículo no encontrado"
 
+    await ctx.send(texto)
+@bot.command(name='exclusivos')
+async def consulta_ex(ctx):
+    texto = "**- Baller 6 \n- Blazer 3\n- Chimera\n- Dukes 3\n- Issi 7\n- Novak\- Seasparrow 2**"
     await ctx.send(texto)
 bot.run(os.environ['tokendiscord'])

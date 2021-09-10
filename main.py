f0d83718b64e22c5b89da7d652c2462e318052cb
Scott <ccssggaa@gmail.com>
2021-09-10 16:52:43 -0500

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ async def consulta_precio(ctx, nombre):
     lower = nombre.lower()
     with open('autoscsvtxt.txt', newline='') as File:
         datos = File.readlines()
-        texto = "Hola, gracias por preferirnos. \nTodo parece indicar que aún no contamos con " + nombre + "."
+        texto = "Hola, gracias por preferirnos. \nTodo parece indicar que aún no contamos con __" + nombre + "__."
         for row in datos:
             if lower in row:
                 x = row.split(",")
@@ -22,6 +22,11 @@ async def consulta_precio(ctx, nombre):
                     pu=int(x[6])
                     ll=int(x[7])
                     texto = salida.format(papeles+ (h* hierro) + (p*plastico) + (v* vidrio) + pu+ll)
+                elif lower=="effy":
+                    texto = "Hola, ella no tiene precio, por más dinero que tengas nunca te alcanzará c:"
+                elif lower == "lavin":
+                    texto = "Hola, esa muchacha es gratis, cualquiera puede tenerla."
+
 
     await ctx.send(texto)
 @bot.command(name='exclusivos')

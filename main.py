ab0e28d1b80862cc9857ba9b0af50297b67cb73e
Scott <ccssggaa@gmail.com>
2021-09-26 12:19:20 -0500

    UPDATE

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ plastico = 180
 vidrio = 180
 bot = commands.Bot(command_prefix ='/')
 slash = SlashCommand(bot, sync_commands=True)
+def regla(material, porcentaje):
+    total = (porcentaje*material) / 100
+    return material - int(total)
+
 @bot.command(name='precio')
 async def consulta_precio(ctx, nombre):
     lower = nombre.lower()
@@ -136,5 +140,56 @@ async def consulta_precio(ctx, nombre):
         embed = discord.Embed(title="**" + nombre.capitalize() + "**", description="Esa muchacha no es para cualquiera, vale mucho <3. \n(No hay foto para que no te enamores)", colour=0x13D8)
         #embed.set_image(url=img)
     await ctx.send(embed=embed)
+@slash.slash(name='precioconmaterial',  description="Muestra el precio total del vehículo proporcionando materiales")
+async def consulta_precio(ctx, nombre, porcentaje):
+    lower = nombre.lower()
+    with open('autoscsvtxt.txt', newline='') as File:
+        datos = File.readlines()
+        embed = discord.Embed(title="**Hola.**", description="Todo parece indicar que aún no contamos con "+nombre.capitalize()+".\nFord Motor Company agradece tu preferencia.", colour=0x13D8)
 
+        for row in datos:
+            if lower in row:
+                x = row.split(",")
+                if(x[0]==lower):
+                    salida = "** Vehículo: " + x[0].capitalize() + "**\n\t- Tipo: "+ x[1]+ "\n\t- Precio Total: **${:,}.** "
+                    salida2 = "\n\tPrecio aportando materiales: **${:,}.**"
+                    papeles = int(x[2])
+                    h=int(x[3])
+                    p=int(x[4])
+                    v=int(x[5])
+                    pu=int(x[6])
+                    ll=int(x[7])
+                    totalh = regla(h,porcentaje)
+                    totalp = regla(p,porcentaje)
+                    totalv = regla(v, porcentaje)
+                    texto = salida.format(papeles + (totalh* hierro) + ( totalp  * plastico) + (totalv * vidrio) + pu+ll)
+                    img = "\nhttps://site-static.up-cdn.com/modules/gtav/vehiculos/res/vehicles/"+lower+".png"
+                    if img:
+                        texto += img
+                    embed = discord.Embed(title="**"+nombre.capitalize()+"**",  description="\tTipo: " +x[1], colour=0x13D8)
+                    embed.add_field(name="*Incluye costos de logística*", value = salida2.format(papeles + (h * hierro) + (p*plastico) + (v * vidrio) + pu+ll), inline = False)
+                    embed.set_image(url=img)
+
+                    if (porcentaje > 50):
+                        embed = discord.Embed(title="**Hola.**",
+                                              description="La cantidad máxima de materiales es 50%.\nFord Motor Company agradece tu preferencia.",
+                                              colour=0x13D8)
+                    if (porcentaje <=0):
+                        embed = discord.Embed(title="**Hola.**",
+                                              description="La cantidad no es válida. Utliza **/precio _nombrevehículo_**\nFord Motor Company agradece tu preferencia.",
+                                              colour=0x13D8)
+
+    if lower == "effy":
+        img="https://cdn.discordapp.com/avatars/439245514060464128/484c45c34c3db6232de8b1151d6071e3.png?size=512"
+        embed = discord.Embed(title="**" + nombre.capitalize() + "**", description="Hola, ella no tiene precio, por más dinero que tengas nunca te alcanzará c:", colour=0x13D8)
+        embed.set_image(url=img)
+    elif lower == "lavin":
+        img = "https://site-static.up-cdn.com/f/7e65ffa29ed72bf6902b2068f13931ebf0148d415e03cc6e42af521165ac72ec04e91cd5488cbd45/960x960"
+        embed = discord.Embed(title="**" + nombre.capitalize() + "**", description="Esa muchacha es gratis, cualquiera puede tenerla.", colour=0x13D8)
+        embed.set_image(url=img)
+    elif lower == "calleja":
+       # img = "https://site-static.up-cdn.com/f/7e65ffa29ed72bf6902b2068f13931ebf0148d415e03cc6e42af521165ac72ec04e91cd5488cbd45/960x960"
+        embed = discord.Embed(title="**" + nombre.capitalize() + "**", description="Esa muchacha no es para cualquiera, vale mucho <3. \n(No hay foto para que no te enamores)", colour=0x13D8)
+        #embed.set_image(url=img)
+    await ctx.send(embed=embed)
 bot.run(os.environ['tokendiscord'])
